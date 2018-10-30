21c0cab11c61b886f3f5465b5e189900e319249e
Md. Arif Ahmed <32490375+mdarifa@users.noreply.github.com>
2018-10-31 00:23:12 +0600

    Update chace clear.php

--- Important Scripts/chace clear.php
+++ Important Scripts/chace clear.php
@@ -4,3 +4,6 @@ Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('config:cache');
     return 'DONE'; //Return anything
 });
+
+
+#first add this code in web.php file then from browser access the route
