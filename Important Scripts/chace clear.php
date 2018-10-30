cff2cf94ced430339d5de112c5ed5bc357aaa5d1
mdarifa <mdarifahmed01@gmail.com>
2018-10-31 00:21:37 +0600

    Important script

---
+++ Important Scripts/chace clear.php
@@ -0,0 +1,6 @@
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
